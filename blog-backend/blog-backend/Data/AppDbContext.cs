ae3c2e9977c4b2b6735f9551403d88c70fdaa52a
hashantr99 <123436883+hashantr99@users.noreply.github.com>
2026-01-23 01:54:09 +0530

    Code first Db Context,Entity & DB Migration

---
+++ blog-backend/blog-backend/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using blog_backend.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace blog_backend.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) { }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ blog-backend/blog-backend/Entity/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace blog_backend.Entity
+{
+    [Table("Categories")]
+    public class Category
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+    }
+}
