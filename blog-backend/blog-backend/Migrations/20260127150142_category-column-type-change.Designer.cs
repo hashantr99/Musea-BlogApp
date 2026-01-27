47dc7cbf9b60ffd5c7c8f5eb66a85723d40fef55
hashantr99 <123436883+hashantr99@users.noreply.github.com>
2026-01-27 20:45:05 +0530

    category_controller_api

--- blog-backend/blog-backend/Migrations/20260127150142_category-column-type-change.Designer.cs
+++ blog-backend/blog-backend/Migrations/20260127150142_category-column-type-change.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using blog_backend.Data;
 
@@ -10,9 +11,11 @@ using blog_backend.Data;
 namespace blog_backend.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260127150142_category-column-type-change")]
+    partial class categorycolumntypechange
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
