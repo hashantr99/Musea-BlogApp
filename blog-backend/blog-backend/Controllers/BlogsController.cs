b842a1f002e171a891733ea00dbb3d50213b741f
hashantr99 <123436883+hashantr99@users.noreply.github.com>
2026-01-28 21:12:19 +0530

    get_blogs_list_api

---
+++ blog-backend/blog-backend/Controllers/BlogsController.cs
@@ -0,0 +1,26 @@
+﻿using blog_backend.Data;
+using blog_backend.Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace blog_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsController : ControllerBase
+    {
+        private readonly IRepository<Blog> _blogRepository;
+
+        public BlogsController(IRepository<Blog> blogRepository) 
+        {
+            _blogRepository = blogRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetBlogsList()
+        {
+            var blogs = await _blogRepository.GetAll();
+            return Ok(blogs);
+        }
+    }
+}
