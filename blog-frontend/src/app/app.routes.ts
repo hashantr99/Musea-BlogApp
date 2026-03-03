97c0a7535a2a80b5edcd43341a864e4f98d9d24e
hashantr99 <123436883+hashantr99@users.noreply.github.com>
2026-03-03 20:33:15 +0530

    admin_dashboard

--- blog-frontend/src/app/app.routes.ts
+++ blog-frontend/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './components/home/home.component';
 import { BlogsComponent } from './components/blogs/blogs.component';
 import { BlogComponent } from './components/blog/blog.component';
 import { AboutComponent } from './components/about/about.component';
+import { ManageBlogsComponent } from './components/admin/manage-blogs/manage-blogs.component';
 
 export const routes: Routes = [
     {
@@ -20,5 +21,9 @@ export const routes: Routes = [
     {
         path: "about",
         component: AboutComponent
+    },
+    {
+        path: "admin/blogs",
+        component: ManageBlogsComponent
     }
 ];
